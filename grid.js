33f54c7f4e2dd2c24c1706c1f44eafae4fafa912
Theo Lima <theo.suporte@hotmail.com>
2022-03-08 20:49:58 -0300

    Builds and model the base squares and grid

---
+++ grid.js
@@ -0,0 +1,7 @@
+const GRID_SIZE = 4;
+const CELL_SIZE = 20;
+const CELL_GAP = 2;
+
+export default class Grid {
+  constructor(gridElement) {}
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+import Grid from './Grid.js';
+const gameBoard = document.GetElementById('game-board');
+
+const grid = new Grid(gameboard);
