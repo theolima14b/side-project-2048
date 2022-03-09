91105c4e4c77ae7d61739dca169874d82d1709c9
Theo Lima <theo.suporte@hotmail.com>
2022-03-08 23:28:06 -0300

    Finish 2048

--- Grid.js
+++ Grid.js
@@ -1,21 +1,41 @@
-const GRID_SIZE = 4;
-const CELL_SIZE = 20;
-const CELL_GAP = 2;
+const GRID_SIZE = 4
+const CELL_SIZE = 20
+const CELL_GAP = 2
 
 export default class Grid {
-  #cells 
+  #cells
 
   constructor(gridElement) {
-    gridElement.style.setProperty("--grid-size", GRID_SIZE);
-    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
-    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+    gridElement.style.setProperty("--grid-size", GRID_SIZE)
+    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
+    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
     this.#cells = createCellElements(gridElement).map((cellElement, index) => {
-      return new Cell (
-        cellElement, 
-        index % GRID_SIZE, 
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,
         Math.floor(index / GRID_SIZE)
-        );
+      )
     })
+  }
+
+  get cells() {
+    return this.#cells
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || []
+      cellGrid[cell.y][cell.x] = cell
+      return cellGrid
+    }, [])
+  }
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || []
+      cellGrid[cell.x][cell.y] = cell
+      return cellGrid
+    }, [])
   }
 
   get #emptyCells() {
@@ -23,21 +43,30 @@ export default class Grid {
   }
 
   randomEmptyCell() {
-    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
-    return this.#emptyCells[randomIndex];
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
+    return this.#emptyCells[randomIndex]
   }
-};
+}
 
 class Cell {
   #cellElement
   #x
   #y
   #tile
+  #mergeTile
 
   constructor(cellElement, x, y) {
-    this.#cellElement = cellElement;
-    this.#x = x;
-    this.#y = y;
+    this.#cellElement = cellElement
+    this.#x = x
+    this.#y = y
+  }
+
+  get x() {
+    return this.#x
+  }
+
+  get y() {
+    return this.#y
   }
 
   get tile() {
@@ -50,15 +79,40 @@ class Cell {
     this.#tile.x = this.#x
     this.#tile.y = this.#y
   }
+
+  get mergeTile() {
+    return this.#mergeTile
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value
+    if (value == null) return
+    this.#mergeTile.x = this.#x
+    this.#mergeTile.y = this.#y
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    )
+  }
+
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return
+    this.tile.value = this.tile.value + this.mergeTile.value
+    this.mergeTile.remove()
+    this.mergeTile = null
+  }
 }
 
 function createCellElements(gridElement) {
-  const cells = [];
+  const cells = []
   for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cells.push(cell);
-    gridElement.append(cell);
+    const cell = document.createElement("div")
+    cell.classList.add("cell")
+    cells.push(cell)
+    gridElement.append(cell)
   }
-  return cells;
+  return cells
 }
--- Tile.js
+++ Tile.js
@@ -4,27 +4,31 @@ export default class Tile {
   #y
   #value
 
-  constructor(tileContainer, value = Math.random() > .5 ? 2 : 4) {
-    this.#tileElement = document.createElement('div');
-    this.#tileElement.classList.add('tile');
-    tileContainer.append(this.#tileElement);
-    this.value = value;
+  constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
+    this.#tileElement = document.createElement("div")
+    this.#tileElement.classList.add("tile")
+    tileContainer.append(this.#tileElement)
+    this.value = value
+  }
+
+  get value() {
+    return this.#value
   }
 
   set value(v) {
-    this.#value = v;
-    this.#tileElement.textContent = v;
-    const power = Math.log2(v);
+    this.#value = v
+    this.#tileElement.textContent = v
+    const power = Math.log2(v)
     const backgroundLightness = 100 - power * 9
     this.#tileElement.style.setProperty(
-      '--background-lightness', 
+      "--background-lightness",
       `${backgroundLightness}%`
     )
     this.#tileElement.style.setProperty(
-      '--text-lightness', 
+      "--text-lightness",
       `${backgroundLightness <= 50 ? 90 : 10}%`
     )
-  } 
+  }
 
   set x(value) {
     this.#x = value
@@ -34,5 +38,21 @@ export default class Tile {
   set y(value) {
     this.#y = value
     this.#tileElement.style.setProperty("--y", value)
+  }
+
+  remove() {
+    this.#tileElement.remove()
+  }
+
+  waitForTransition(animation = false) {
+    return new Promise(resolve => {
+      this.#tileElement.addEventListener(
+        animation ? "animationend" : "transitionend",
+        resolve,
+        {
+          once: true,
+        }
+      )
+    })
   }
 }
